aae216b71439c931e4410a74158d3ab09271864b
Somil Kaushik <somil.kaushik@somilkaushikss219.successivetech.com>
2023-11-08 00:27:16 -0500

    assignment-5

--- react_training/src/Assignment5/Ques12/Sample.jsx
+++ react_training/src/Assignment5/Ques12/Sample.jsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import Button from './Button';
+import React from "react";
+import Button from "./Button";
 
 const Sample = () => {
-    const handleClickPrimary=()=>{
-        alert("Primary Button");
-    }
-    const handleClickSecondary=()=>{
-        alert("Secondary Button");
-    }
-    const handleClickDanger=()=>{
-        alert("Danger Button");
-    }
+  const handleClickPrimary = () => {
+    alert("Primary Button");
+  };
+  const handleClickSecondary = () => {
+    alert("Secondary Button");
+  };
+  const handleClickDanger = () => {
+    alert("Danger Button");
+  };
   return (
     <>
-    <p>Sample Buttons:</p>
-    <Button styles="primary" onClick={handleClickPrimary} >Primary</Button>
-    <br/><br/>
-    <Button styles="secondary" onClick={handleClickSecondary} >Secondary</Button>
-    <br/><br/>
-    <Button styles="danger" onClick={handleClickDanger} >Danger</Button>
-    
+      <p>Sample Buttons:</p>
+      <Button styles="primary" onClick={handleClickPrimary}>
+        Primary
+      </Button>
+      <br />
+      <br />
+      <Button styles="secondary" onClick={handleClickSecondary}>
+        Secondary
+      </Button>
+      <br />
+      <br />
+      <Button styles="danger" onClick={handleClickDanger}>
+        Danger
+      </Button>
     </>
-  )
-}
+  );
+};
 
 export default Sample;
