15072cac96863983976b2bcaa7763f5f5b6ed3b2
Somil Kaushik <somil.kaushik@somilkaushikss219.successivetech.com>
2023-11-08 03:36:50 -0500

    resolved conflicts

--- react_training/src/Routing.jsx
+++ react_training/src/Routing.jsx
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MainPage from "./MainPage";
-
-import Assignment_3 from "./Assignment3/Assignment_3";
-const Routing = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-
-          <Route path="/assignment3" element={<Assignment_3 />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-};
-export default Routing;
-=======
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MainPage from './MainPage';
 import Assignment_1 from './Assignment1/Assignment_1';
@@ -37,4 +17,3 @@ const Routing = ()=>{
     )
 }
 export default Routing;
->>>>>>> 1e3d4eb95aa669847822993d68a1f421ab327d4b
