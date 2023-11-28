4989dae467e0977bfb60a403704fde5be253add2
somil-successive <146409919+somil-successive@users.noreply.github.com>
2023-11-28 10:25:02 +0530

    assignment 6 #133955 (#6)
    * Assignment-6
    
    * Assignment-6
    
    * Assignment-6
    
    * changes
    
    * Updated Assignment-6
    
    ---------
    
    Co-authored-by: Somil Kaushik <somil.kaushik@somilkaushikss219.successivetech.com>
    Co-authored-by: Somil Kaushik <somil.kaushik@somilkss297.successivetech.com>

--- react_training/src/Routing.jsx
+++ react_training/src/Routing.jsx
@@ -20,6 +20,7 @@ import Inbox from "./Assignment4/Ques9/Inbox";
 import Assignment_4 from "./Assignment4/Assignment_4";
 import Assignment_5 from "./Assignment5/Assignment_5";
 import HomeQ14 from "./Assignment5/Question14.3";
+import Assignment_6 from "./Assignment6/Assignment_6";
 const Routing = () => {
   return (
     <>
@@ -27,19 +28,14 @@ const Routing = () => {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/assignment1" element={<Assignment_1 />} />
-
           <Route path="/assignment2" element={<Assignment_2 />} />
-
           <Route path="/assignment3" element={<Assignment_3 />} />
-
           <Route path="/assignment3/home" element={<Home />} />
           <Route path="/assignment3/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
-
           <Route path="/assignment4/home" element={<HomeQ4 />} />
           <Route path="/assignment4/login" element={<LoginPage />} />
           <Route path="/assignment4/about" element={<AboutQ4 />} />
-
           <Route path="/assignment5" element={<Dashboard />} />
           <Route path="/assignment5/profile" element={<Profile />} />
           <Route path="/assignment5/setting" element={<Setting />} />
@@ -51,6 +47,7 @@ const Routing = () => {
           <Route path="/assignment5/q9/inbox" element={<Inbox />} />
           <Route path="/assign5" element={<Assignment_5 />} />
           <Route path="/assignment-5/Ques-14" element={<HomeQ14 />} />
+          <Route path="/assignment6" element={<Assignment_6 />} />
         </Routes>
       </BrowserRouter>
     </>
